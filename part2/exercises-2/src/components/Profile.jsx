ad800657539429b504c909a1678ef002f90c20f1
Carolyn Smith <carolynsmith@slu.edu>
2024-08-20 09:56:29 -0500

    exercises part 2

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,14 +1,28 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
+let listItem = oceans.map(oceans=>
+   <div key={oceans.id} className={`${oceans.fishCheck === "true" ? "isAFish" : "profile"}`}>
+      <img src={oceans.image} alt={oceans.name} className="img"/>
+      <h1>{oceans.name}</h1>
+         <h3>Fun Facts:</h3>
+            <ul>
+               <li>{oceans.fact1}</li>
+               <li>{oceans.fact2}</li>
+               <li>{oceans.fact3}</li>
+            </ul>
+            <Button /> 
+   </div>
+   
+)
 
 function Profile()
 {
-   return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+   return (
+         <ul>
+            {listItem}
+         </ul>
    );
 }
 
